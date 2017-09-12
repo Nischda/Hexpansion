30dfe48e379d659854c725bbdaf9dc6f32f6f3c8
TimB <tim_beier@hotmail.de>
2017-09-12 15:50:49 +0200

    Now generates Map of hexagons which can be clicked to switch material color

---
+++ Hexpansion/Assets/Scripts/HexMap.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexMap : MonoBehaviour
+{
+
+	public GameObject HexTilePrefab;
+
+	//number of tiles per axis.
+	private int _width = 20;
+	private int _height = 20;
+
+	private float _xOffset = 1f;
+	private float _zOffset = 0.86f;
+	void Start () {
+		CreateHexMap();
+	}
+	
+	void Update () {
+		
+	}
+
+	private void CreateHexMap() {
+		for (int x = 0; x < _width; x++) {
+			for (int y = 0; y < _height; y++) {
+				
+				float xPos = x * _xOffset;
+
+				if (y % 2 == 1) {
+					xPos += _xOffset / 2f;
+				}
+				GameObject hexTileGameObject = Instantiate(HexTilePrefab, new Vector3(xPos , 0, y * _zOffset), Quaternion.identity);
+				hexTileGameObject.name = "Hex_" + x + "_" + y;
+				hexTileGameObject.transform.SetParent(this.transform);
+				hexTileGameObject.isStatic = true;
+				
+				hexTileGameObject. transform.localScale += new Vector3(0, Random.value*10, 0);
+			}
+		}
+	}
+}
---
+++ Hexpansion/Assets/Scripts/MouseManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour {
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hitInfo;
+		if (Physics.Raycast(ray, out hitInfo)) {
+			GameObject ourhitObject = hitInfo.collider.transform.gameObject;
+			
+			if (Input.GetMouseButtonDown(0)) { //left mouse button clicked
+				MeshRenderer mr = ourhitObject.GetComponentInChildren<MeshRenderer>();
+
+				if (mr.material.color == Color.red){
+					mr.material.color = Color.white;
+				}
+				else{
+					mr.material.color = Color.red;
+				}
+			}
+		}
+		//Ray fpsRay = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+	}
+}
